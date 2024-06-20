4e840d7fa2af60a1df4149b665e837ffb456fd8d
DELL <DELL@DESKTOP-AM5CF8U>
2024-06-20 14:32:50 +0530

    commit1

---
+++ Jenkins/src/main/java/jenkinspgm/Example1.java
@@ -0,0 +1,10 @@
+package jenkinspgm;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to jenkins");
+	}
+
+}
